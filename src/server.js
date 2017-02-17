a03d2e6c971d11329479eba550b5db48e7e6519a
Madison Smith <ms8565@rit.edu>
2017-02-17 13:49:45 -0500

    CAT CHAT

---
+++ src/server.js
@@ -0,0 +1,156 @@
+'use strict';
+
+const http = require('http');
+const fs = require('fs');
+const socketio = require('socket.io');
+
+const users = {};
+
+const catPhrases = [
+  'Meow meow meow meow meow meow.',
+  'MrOOOOw!',
+  '*Blep*',
+  '*Blinks once*',
+  '*Blinks twice*',
+  'Meow.',
+  '*Judgmental stare*',
+  'Meow meow meow!',
+  'Nya~',
+  'Meow, meow meow meow, meow meow!',
+  '*Flips tail*',
+  '*Chases own tail*',
+  '*Infernal caterwauling*',
+  'JELLICLE SONGS FOR JELLICLE CATS! *does a handstand dance routine',
+  'PuuurrrrrrRRrrr.',
+  'HHHIIIIiiiSSSSSSS',
+  'Meeooooooow.',
+  '*soft yawn*',
+  '*Kneads lap*',
+  '*Kneads lap wITH CLAWS OW*',
+  '*Attacks you unexpectedly, even though you were JUST PETTING THEM?? WHY??*',
+  'Shimmy shimmy *pounce*',
+  'Chirp chirrppp (but in that weird cat way)',
+  'MEOW. MEOW MEOW. Meeooowww... MEOW!',
+  'MeeeeOW???',
+];
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+
+const onRequest = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(index);
+  response.end();
+};
+
+const app = http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
+
+const io = socketio(app);
+const serverAvatar = 'https://people.rit.edu/ms8565/realtime/images/serverCat.jpg';
+
+const sendMsg = (message, name, color, avatar, catMsg, timestamp) => {
+  let nameTemp = name;
+  let catMsgTemp = catMsg;
+  let timestampTemp = timestamp;
+  let avatarTemp = avatar;
+  let colorTemp = color;
+
+  if (name === undefined) nameTemp = 'Server Cat';
+  if (color === undefined) colorTemp = 'black';
+  if (catMsg === undefined) catMsgTemp = '0101011010 :3 0101010';
+  if (timestamp === undefined) timestampTemp = new Date();
+  if (avatar === undefined) avatarTemp = serverAvatar;
+
+  return {
+    name: nameTemp,
+    msg: message,
+    color: colorTemp,
+    avatar: avatarTemp,
+    catMsg: catMsgTemp,
+    timestamp: timestampTemp,
+  };
+};
+
+const onJoined = (sock) => {
+  const socket = sock;
+
+  socket.on('join', (data) => {
+    socket.name = data.name;
+    socket.color = data.color;
+    socket.avatar = data.avatar;
+
+    users[socket.id] = socket;
+
+    socket.emit('msg', sendMsg(`There are ${Object.keys(users).length + 1} users`));
+
+    socket.join('room1');
+
+    socket.broadcast.to('room1').emit('msg', sendMsg(`${data.name} has joined the room.`));
+    console.log(`${data.name} joined`);
+
+    socket.emit('msg', sendMsg('You joined the room!'));
+  });
+};
+
+const onMsg = (sock) => {
+  const socket = sock;
+  const date = new Date();
+
+  socket.on('msgToServer', (data) => {
+    const catPhrase = catPhrases[Math.floor(Math.random() * catPhrases.length)];
+    io.sockets.in('room1').emit('msg', sendMsg(data.msg, socket.name, socket.color, socket.avatar, catPhrase, date));
+  });
+};
+
+const onTime = (sock) => {
+  const socket = sock;
+
+  socket.on('getTime', () => {
+    const date = new Date();
+
+    io.to(socket.id).emit('msg', sendMsg(`The date is ${date}`));
+  });
+};
+
+
+const onChangeAttributes = (sock) => {
+  const socket = sock;
+
+  socket.on('changeName', (data) => {
+    socket.emit('msg', sendMsg(`${socket.name} is now ${data.name}`));
+    socket.name = data.name;
+  });
+  socket.on('changeColor', (data) => {
+    socket.color = data.color;
+  });
+  socket.on('changeAvatar', (data) => {
+    socket.avatar = data.avatar;
+  });
+};
+
+const onDisconnect = (sock) => {
+  const socket = sock;
+
+  socket.on('disconnect', () => {
+    socket.broadcast.to('room1').emit('msg', sendMsg(`${socket.name} has left`));
+
+    socket.leave('room1');
+
+    delete users[socket.id];
+  });
+};
+
+io.sockets.on('connection', (socket) => {
+  console.log('started');
+
+  onJoined(socket);
+  onMsg(socket);
+  onChangeAttributes(socket);
+  onTime(socket);
+  onDisconnect(socket);
+});
+
+console.log('Websocket server started');
