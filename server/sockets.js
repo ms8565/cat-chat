480bc0854a533cd60e2b545a445d6d19a6ebe3cd
Madison Smith <ms8565@rit.edu>
2017-05-01 12:17:01 -0400

    Part 1 of byte array serialization -Will currently break CircleCI -All functions are implemented except for client serialization

--- server/sockets.js
+++ server/sockets.js
@@ -1,4 +1,5 @@
-
+'use strict';
+const xxh = require('xxhashjs');
 const users = {};
 
 let io;
@@ -33,7 +34,7 @@ const catPhrases = [
 
 const serverAvatar = 'https://people.rit.edu/ms8565/realtime/images/serverCat.jpg';
 
-const sendMsg = (message, name, color, avatar, catMsg, timestamp) => {
+const sendMsg = (hash, message, name, color, avatar, catMsg, timestamp) => {
   let nameTemp = name;
   let catMsgTemp = catMsg;
   let timestampTemp = timestamp;
@@ -47,6 +48,7 @@ const sendMsg = (message, name, color, avatar, catMsg, timestamp) => {
   if (avatar === undefined) avatarTemp = serverAvatar;
 
   return {
+    hash: hash,
     name: nameTemp,
     msg: message,
     color: colorTemp,
@@ -56,6 +58,132 @@ const sendMsg = (message, name, color, avatar, catMsg, timestamp) => {
   };
 };
 
+const encodeMessage = (message) => {
+  
+  let bufferObjs = [];
+  //get the byte value from our hash (character string id)
+    //This will convert the character hash to bytes that we can
+    //send in our byte array
+    const hashBuffer = Buffer.from(msgObj.hash, 'utf-8');
+    //get the length of our buffer so we can add it to our total to allocate
+    const hashLength = hashBuffer.byteLength;
+    //add to our total length. We need to know the exact number of bytes we are sending.
+    bufferObjs.push({
+      buffer: hashBuffer,
+      length: hashLength
+    })
+    
+    
+    const nameBuffer = Buffer.from(msgObj.name, 'utf-8');
+    const nameLength = nameBuffer.byteLength;
+    bufferObjs.push({
+      buffer: nameBuffer,
+      length: nameLength
+    });
+    
+    const msgBuffer = Buffer.from(msgObj.msg, 'utf-8');
+    const msgLength = msgBuffer.byteLength;
+    totalLength += msgLength;
+    bufferObjs.push({
+      buffer: msgBuffer,
+      length: msgLength
+    });
+  
+    const colorBuffer = Buffer.from(msgObj.color, 'utf-8');
+    const colorLength = colorBuffer.byteLength;
+    bufferObjs.push({
+      buffer: colorBuffer,
+      length: colorLength
+    });
+  
+    const avatarBuffer = Buffer.from(msgObj.avatar, 'utf-8');
+    const avatarLength = avatarBuffer.byteLength;
+    bufferObjs.push({
+      buffer: avatarBuffer,
+      length: avatarLength
+    });
+    
+    const catMsgBuffer = Buffer.from(msgObj.catMsg, 'utf-8');
+    const catMsgLength = catMsgBuffer.byteLength;
+    bufferObjs.push({
+      buffer: catMsgBuffer,
+      length: catMsgLength
+    });
+    
+    //allocate a buffer (byte array) of 8 bytes that we can store a date value in
+    //Dates are stored as a double (8 bytes) so that tells us how much to allocate
+    const dateBuffer = Buffer.alloc(8); //8 bytes in a double
+    //write double, read on the client as getFloat64 from dataview
+    
+    dateBuffer.writeDoubleBE(msgObj.timestamp);
+    //get the byte length of our date 
+    const dateLength = dateBuffer.byteLength;
+    bufferObjs.push({
+      buffer: dateBuffer,
+      length: dateLength
+    });
+    
+    
+    let totalLength = bufferObjs.length; //Add 1 per every element, since the length of the buffer will be sent
+    for(let i = 0; i < bufferObjs.length; i++){
+      totalLength += bufferObjs[i].length; //Add the lengths of all buffers
+    }
+    
+    let offset = 0;
+    let message = Buffer.alloc(totalLength);
+    for(let i = 0; i < bufferObjs.length; i++){
+      message.writeInt8(bufferObjs[i].length, offset);
+      offset += 1;
+      
+      bufferObjs[i].buffer.copy(message, offset);
+      offset += bufferObjs[i].length;
+    }
+
+    //return serialized message
+    return message;
+};
+
+const decodeMessage = (data) => {
+  //convert into a buffer
+  const buffer = Buffer.from(data, 'utf-8');
+  let totalOffset = 0;
+  
+  //Decode length of message
+  const messageLength = buffer.readInt8(totalOffset);
+  totalOffset += 1;
+
+  //Decode message
+  const message = buffer.toString('utf-8', totalOffset, totalOffset+messageLength);
+  totalOffset += messageLength;
+  
+  //Decode length of date
+  const dateLength = buffer.readInt8(totalOffset);
+  totalOffset += 1;
+
+  //Decode date
+  const date = buffer.toString('utf-8', totalOffset, totalOffset+dateLength);
+  
+  return {msg: message, timestamp: date};
+};
+
+const decodeAttribute = (data) => {
+  //convert into a buffer
+  const buffer = Buffer.from(data, 'utf-8');
+  let totalOffset = 0;
+  
+  //Decode length of attribute
+  const attributeLength = buffer.readInt8(totalOffset);
+  totalOffset += 1;
+
+  //Decode attribute
+  const attribute = buffer.toString('utf-8', totalOffset, totalOffset+attributeLength);
+  
+  return attribute;
+}
+
+
+
+
 const onJoined = (sock) => {
   const socket = sock;
 
@@ -63,12 +191,14 @@ const onJoined = (sock) => {
     socket.name = data.name;
     socket.color = data.color;
     socket.avatar = data.avatar;
+    
+    const hash = xxh.h32(`${socket.id}${new Date().getTime()}`, 0xCAFEBABE).toString(16);
 
-    users[socket.id] = socket;
+    users[hash] = socket;
+    
+    socket.join('room1');
 
     socket.emit('msg', sendMsg(`There are ${Object.keys(users).length + 1} users`));
-
-    socket.join('room1');
 
     socket.broadcast.to('room1').emit('msg', sendMsg(`${data.name} has joined the room.`));
     console.log(`${data.name} joined`);
